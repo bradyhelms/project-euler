8b775b622e2f746a0c04991d542e20cf3a605f07
Brady Helms <bradyhelms@gmail.com>
2024-02-28 16:42:00 -0500

    reorganize and add problem generator

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 My solutions to the Project Euler questions. Mostly done in Ruby, not the most efficient for many of these problems, but it's what I like using.
 
 I have done many of these problems in other languages (mostly perl). In the past, I frequently attempted to solve everything without using any external libraries/packages. In this repo, I'm using this as an oppurtunity to become more acquainted with modern packages. 
+
+There is a file called autoGenerateFiles.rb that crudely creates files contaning the problem statement and the appropriate file marker. Edit as needed if you'd like to reuse.
---
+++ autoGenerateFiles.rb
@@ -0,0 +1,35 @@
+require 'net/http'
+require 'uri'
+
+def main
+  # Set start and end problems that you want to create
+  start_problem = 1
+  max_problem   = 5
+
+  (start_problem..max_problem).each do |i|
+    puts "Creating file problem#{i}.rb"
+    page = removeHTML(getPage(i)) 
+    file = File.new("problem#{i}.rb", "w")
+    file.puts(page)
+    file.close
+  end
+end
+
+def getPage(prob_number)
+  uri = URI.parse("https://projecteuler.net/minimal=#{prob_number}")
+  response = Net::HTTP.get_response(uri)
+  return response.body
+end
+
+## Remove some of the HTML garbage. 
+#   Not perfect, but makes everything
+#   look about 1% better
+def removeHTML(html_str)
+  html_str.gsub!(/(<[^>]*>)|\n|\t|\$/s, "")
+  
+  # Also add multiline comment markers for Ruby!
+  return "=begin\n" + html_str + "\n=end"
+
+end
+
+main
